bcb71c921c751e18a99e3fd2beb492ed2924ef20
Lin Mulan <admin@snake1999.com>
2018-04-07 14:04:05 +0800

    First test class

---
+++ blackbird-core/src/test/kotlin/io/nukkit/blackbird/test/tests.kt
@@ -0,0 +1,11 @@
+package io.nukkit.blackbird.test
+
+import org.junit.Test as test
+import org.junit.Assert.*
+
+class TestSource {
+    @test fun itWorks() {
+        println("Hello world, I am Blackbird!")
+        assertEquals(2+2, 4)
+    }
+}
